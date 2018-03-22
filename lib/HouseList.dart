2b9d9a6e364e5938e04dba49f4b34e84109cdc2a
im-minion <vaibhavminiyar@gmail.com>
2018-03-22 16:24:48 +0530

    card size changes

--- lib/HouseList.dart
+++ lib/HouseList.dart
@@ -5,6 +5,14 @@ class HouseList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
+    var size = MediaQuery
+        .of(context)
+        .size;
+
+    /*24 is for notification bar on Android*/
+    final double itemHeight = (size.height - kToolbarHeight - 24) / 2;
+    final double itemWidth = size.width / 2;
+
     Card myCard(String houseName, String houseImageUrl) {
       return new Card(
           elevation: 5.0,
@@ -31,10 +39,10 @@ class HouseList extends StatelessWidget {
             ),
             onTap: () {
               Navigator.of(context).push(
-                    new MaterialPageRoute(
-                        builder: (BuildContext context) =>
-                            new SingleHousePage(houseName)),
-                  );
+                new MaterialPageRoute(
+                    builder: (BuildContext context) =>
+                    new SingleHousePage(houseName)),
+              );
             },
           ));
     }
@@ -45,29 +53,34 @@ class HouseList extends StatelessWidget {
               crossAxisSpacing: 10.0,
               crossAxisCount: 2,
               primary: false,
+              shrinkWrap: true,
+              controller: new ScrollController(keepScrollOffset: false),
+              scrollDirection: Axis.vertical,
+              childAspectRatio: (itemWidth / 250.0),
               padding: const EdgeInsets.all(10.0),
+
               children: <Widget>[
-            myCard("House Stark",
-                'https://vignette.wikia.nocookie.net/gameofthrones/images/8/8a/House-Stark-Main-Shield.PNG/revision/latest/scale-to-width-down/350?cb=20170101103142'),
-            myCard("House Arryn",
-                'https://vignette.wikia.nocookie.net/gameofthrones/images/1/15/House-Arryn-Main-Shield.PNG/revision/latest/scale-to-width-down/350?cb=20170101094153'),
-            myCard(
-              "House Tully",
-              'https://vignette.wikia.nocookie.net/gameofthrones/images/b/bd/House-Tully-Main-Shield.PNG/revision/latest/scale-to-width-down/350?cb=20170523040648',
-            ),
-            myCard(
-              "House Baratheon",
-              'http://awoiaf.westeros.org/images/thumb/2/2d/House_Baratheon.svg/250px-House_Baratheon.svg.png',
-            ),
-            myCard(
-              "House Targaryen",
-              'https://vignette.wikia.nocookie.net/gameofthrones/images/4/43/House-Targaryen-Main-Shield.PNG/revision/latest/scale-to-width-down/350?cb=20170510235320',
-            ),
-            myCard(
-              "House Lannister ",
-              'https://vignette.wikia.nocookie.net/gameofthrones/images/8/8a/House-Lannister-Main-Shield.PNG/revision/latest/scale-to-width-down/350?cb=20170101095357',
-            ),
-          ])),
+                myCard("House Stark",
+                    'https://vignette.wikia.nocookie.net/gameofthrones/images/8/8a/House-Stark-Main-Shield.PNG/revision/latest/scale-to-width-down/350?cb=20170101103142'),
+                myCard("House Arryn",
+                    'https://vignette.wikia.nocookie.net/gameofthrones/images/1/15/House-Arryn-Main-Shield.PNG/revision/latest/scale-to-width-down/350?cb=20170101094153'),
+                myCard(
+                  "House Tully",
+                  'https://vignette.wikia.nocookie.net/gameofthrones/images/b/bd/House-Tully-Main-Shield.PNG/revision/latest/scale-to-width-down/350?cb=20170523040648',
+                ),
+                myCard(
+                  "House Baratheon",
+                  'http://awoiaf.westeros.org/images/thumb/2/2d/House_Baratheon.svg/250px-House_Baratheon.svg.png',
+                ),
+                myCard(
+                  "House Targaryen",
+                  'https://vignette.wikia.nocookie.net/gameofthrones/images/4/43/House-Targaryen-Main-Shield.PNG/revision/latest/scale-to-width-down/350?cb=20170510235320',
+                ),
+                myCard(
+                  "House Lannister ",
+                  'https://vignette.wikia.nocookie.net/gameofthrones/images/8/8a/House-Lannister-Main-Shield.PNG/revision/latest/scale-to-width-down/350?cb=20170101095357',
+                ),
+              ])),
     );
   }
 }
