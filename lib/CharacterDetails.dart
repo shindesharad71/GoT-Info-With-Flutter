90813767875fde244eee9f128b00e85cf58c6ee7
Sharad Shinde <shindesharad71@gmail.com>
2018-03-23 00:31:33 +0530

    New character details layout created

--- lib/CharacterDetails.dart
+++ lib/CharacterDetails.dart
@@ -1,12 +1,74 @@
 import 'package:flutter/material.dart';
-class CharDetails extends StatelessWidget{
+
+class CharDetails extends StatelessWidget {
   final String _characterName;
 
   CharDetails(this._characterName);
 
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    return new Text(_characterName);
+    var size = MediaQuery.of(context).size;
+
+    /*24 is for notification bar on Android*/
+    final double itemHeight = (size.height - kToolbarHeight - 24) / 2;
+    final double itemWidth = size.width;
+
+    /* return new Container(
+      child: new Column(
+        children: <Widget>[
+          new Image.network('https://1v1d1e1lmiki1lgcvx32p49h8fe-wpengine.netdna-ssl.com/wp-content/uploads/2016/06/ned-stark-game-of-thrones-740x385.jpg',
+          height: 270.0,
+          width: itemWidth,
+          ),
+          new Text('Ned Stark', style: new TextStyle(
+            fontSize: 28.0,
+            color: Colors.white70,
+          ),
+          textAlign: TextAlign.left,
+          ),
+          new Column(
+
+          ),
+        ],
+      ),
+    ); */
+
+    return new Scaffold(
+      body: new Container(
+        child: new Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            new Padding(
+              padding: const EdgeInsets.fromLTRB(0.0, 24.0, 0.0, 0.0),
+              child: new Image.network(
+                'https://1v1d1e1lmiki1lgcvx32p49h8fe-wpengine.netdna-ssl.com/wp-content/uploads/2016/06/ned-stark-game-of-thrones-740x385.jpg',
+                height: itemHeight - 86.0,
+                width: itemWidth,
+              ),
+            ),
+            new Padding(
+              padding: const EdgeInsets.all(12.0),
+              child: new Text(
+                'Ned Stark (Sean Bean)',
+                style: new TextStyle(
+                  fontSize: 28.0,
+                  color: Colors.black87,
+                ),
+              ),
+            ),
+            new Padding(
+              padding: const EdgeInsets.all(12.0),
+              child: new Text(
+                'Lord Eddard Stark, also known as Ned Stark, was the head of House Stark, the Lord of Winterfell, Lord Paramount and Warden of the North, and later Hand of the King to King Robert I Baratheon. He was the older brother of Benjen, Lyanna and the younger brother of Brandon Stark.',
+                style: new TextStyle(
+                  fontSize: 16.0,
+                  color: Colors.black87,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
