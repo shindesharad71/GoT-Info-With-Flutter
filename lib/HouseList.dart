412e8597cd88faf7cd6d3dc84ca4670af6050719
im-minion <vaibhavminiyar@gmail.com>
2018-03-23 00:58:56 +0530

    used documentation :)

--- lib/HouseList.dart
+++ lib/HouseList.dart
@@ -1,10 +1,31 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:layouts/SingleHousePage.dart' show SingleHousePage;
+import 'dart:io';
+//https://got-flutter.firebaseio.com/houses.json
+Map data;
 
 class HouseList extends StatelessWidget {
+  var httpClient = new HttpClient();
+  get() async{
+    var httpClient = new HttpClient();
+//  var uri = new Uri.http(
+//      'example.com', '/path1/path2', {'param1': '42', 'param2': 'foo'});
+    var uri = new Uri.https('got-flutter.firebaseio.com', '/houses.json');
+    var request = await httpClient.getUrl(uri);
+    var response = await request.close();
+    var responseBody = await response.transform(UTF8.decoder).join();
+    data = JSON.decode(responseBody);
+//    print(data.keys.toList().length);
+  }
+
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
+    get();
+    List<String> housesNameList = data.keys.toList();
+    print(housesNameList[1]);
+
     var size = MediaQuery
         .of(context)
         .size;
@@ -49,7 +70,8 @@ class HouseList extends StatelessWidget {
                     new SingleHousePage(houseName)),
               );
             },
-          ));
+          )
+      );
     }
 
     return new Flexible(
@@ -65,7 +87,8 @@ class HouseList extends StatelessWidget {
               padding: const EdgeInsets.all(10.0),
 
               children: <Widget>[
-                myCard("House Stark",
+                myCard(
+                    housesNameList[1],
                     'https://vignette.wikia.nocookie.net/gameofthrones/images/8/8a/House-Stark-Main-Shield.PNG/revision/latest/scale-to-width-down/350?cb=20170101103142'),
                 myCard("House Arryn",
                     'https://vignette.wikia.nocookie.net/gameofthrones/images/1/15/House-Arryn-Main-Shield.PNG/revision/latest/scale-to-width-down/350?cb=20170101094153'),
@@ -82,10 +105,12 @@ class HouseList extends StatelessWidget {
                   'https://vignette.wikia.nocookie.net/gameofthrones/images/4/43/House-Targaryen-Main-Shield.PNG/revision/latest/scale-to-width-down/350?cb=20170510235320',
                 ),
                 myCard(
-                  "House Lannister ",
+                 housesNameList[0],
                   'https://vignette.wikia.nocookie.net/gameofthrones/images/8/8a/House-Lannister-Main-Shield.PNG/revision/latest/scale-to-width-down/350?cb=20170101095357',
                 ),
-              ])),
+              ]
+          )
+      ),
     );
   }
 }
