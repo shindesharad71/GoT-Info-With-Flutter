93e3054f63e10827b463f637a7d75389f4d04e15
im-minion <vaibhavminiyar@gmail.com>
2018-03-30 23:19:37 +0530

    orientation locked

--- lib/CharacterDetails.dart
+++ lib/CharacterDetails.dart
@@ -1,6 +1,5 @@
 import 'dart:async';
 import 'dart:convert';
-import 'package:flutter/services.dart';
 import 'package:http/http.dart' as http;
 import 'package:flutter/material.dart';
 import 'package:layouts/LoadingPage.dart';
