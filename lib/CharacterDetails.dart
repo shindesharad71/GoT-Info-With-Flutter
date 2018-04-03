314e00f05d6f5d59eb1fcf9fdf196078c9764d76
im-minion <vaibhavminiyar@gmail.com>
2018-04-03 20:19:48 +0530

    done with no connection

--- lib/CharacterDetails.dart
+++ lib/CharacterDetails.dart
@@ -106,7 +106,7 @@ class CharacterDetailsPageState extends State<CharacterDetails> {
             new Padding(
               padding: const EdgeInsets.all(12.0),
               child: new Text(
-                'Character description here',
+                data["description"],
                 style: new TextStyle(
                   fontSize: 16.0,
                   color: Colors.black87,
